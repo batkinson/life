2f399afc2544699cc2cf366e9fd0af535fc79b0a
Brent Atkinson <brent.atkinson@gmail.com>
2014-01-08 06:04:18 +0000

    Initial working curses version.

---
+++ life.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+import random
+                
+class Universe:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.cells = [[random.choice([True, False, False, False]) for x in xrange(width)] for x in xrange(height)]
+
+    def printgrid(self):
+        for row in self.cells:
+            for col in row:
+                print("*" if col else " ", sep=" ", end="")
+            print('')
+
+    def copycells(self):
+        self.oldcells = self.cells
+        self.cells = [[False for x in xrange(self.width)] for x in xrange(self.height)]
+        
+
+    def increment(self):
+        self.copycells()
+        for rowidx in xrange(self.height):
+            for colidx in xrange(self.width):
+                self.updatecell(rowidx, colidx)
+
+    def livecell(self, row, col):
+        return self.oldcells[row][col]
+
+    def liveneighbors(self, row, col):
+        livecount = 0
+        height = self.height
+        width = self.width
+        cells = self.oldcells
+        for rowoffset in [-1, 0, 1]:
+            for coloffset in [-1, 0, 1]:
+                nrow = (row + rowoffset) % height
+                ncol = (col + coloffset) % width
+                if rowoffset != 0 or coloffset != 0:
+                    nlive = self.livecell(nrow, ncol)
+                    # print("    [{},{}], live: {}".format(ncol, nrow, nlive))
+                    if nlive:
+                        livecount += 1
+        return livecount
+            
+    def updatecell(self, row, col):
+        liveneighbors = self.liveneighbors(row, col)
+        if self.livecell(row, col):
+            self.cells[row][col] = liveneighbors >= 2 and liveneighbors <= 3
+        else:
+            self.cells[row][col] = liveneighbors == 3
+        # print("[{},{}], live: {}, neighbors: {}, lives: {}".format(col, row, self.livecell(row, col), liveneighbors, self.cells[row][col]))
+        
+
+
+class Simulation:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.universe = Universe(width, height)
+
+    def defaultdraw(cells):
+        self.universe.printgrid()
+        
+    def run(self, max_iter, drawfunc = defaultdraw):
+        # print("Running {} iterations, dimensions: {}x{}".format(max_iter, self.width, self.height))
+        drawfunc(self.universe.cells)
+        if max_iter is None:
+            while True:
+                self.universe.increment()
+                drawfunc(self.universe.cells)
+        else:
+            for iter in range(1, max_iter + 1):
+                # print("Iteration {}:".format(iter))
+                self.universe.increment()
+                drawfunc(self.universe.cells)
+            
--- main.py
+++ main.py
@@ -1,7 +1,47 @@
-
+import curses
+import sys
+import traceback
+import life
 
 def main():
-  print "Hello World!"
+
+  try:
+    stdscr = curses.initscr()
+    scrheight, scrwidth = stdscr.getmaxyx()
+
+    height = scrheight - 2
+    width = scrwidth - 2
+    
+    iterations = None
+    
+    def cursesdraw(cells):
+      stdscr.border()
+      
+      # fill cells with universe state
+      for y, row in enumerate(cells):
+        for x, col in enumerate(row):
+          stdscr.addch(y+1, x+1, ord('0') if col else ord(' '))
+      stdscr.refresh()
+  
+    simulation = life.Simulation(width, height)
+
+    curses.curs_set(0)
+    curses.noecho()
+    curses.cbreak()
+    
+    simulation.run(iterations, cursesdraw)
+
+  except KeyboardInterrupt:
+    pass
+  except:
+    curses.endwin()
+    print traceback.format_exc()
+    sys.exit(1)
+  finally:
+    curses.nocbreak()
+    curses.echo()
+    curses.endwin()
+    
 
 if __name__ == "__main__":
   main()
