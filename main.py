f70580634ded48ca974263a0560f9727a3178532
Brent Atkinson <brent.atkinson@gmail.com>
2014-04-22 21:41:50 -0400

    Replaced 4 spaces indent for 3.

--- main.py
+++ main.py
@@ -6,53 +6,53 @@ import life
 
 def main():
 
-    usecurses = True
-    
-    if usecurses:
-        stdscr = curses.initscr()
-        scrheight, scrwidth = stdscr.getmaxyx()
-        height, width = scrheight - 2, scrwidth - 2
-        itermax = None
+   usecurses = True
+   
+   if usecurses:
+      stdscr = curses.initscr()
+      scrheight, scrwidth = stdscr.getmaxyx()
+      height, width = scrheight - 2, scrwidth - 2
+      itermax = None
 
-        def cursesdraw(cells):
-            stdscr.erase()
-            stdscr.border()
-            for x, y in cells:
-               stdscr.addch(y+1, x+1, ord('0'))
-            stdscr.refresh()
+      def cursesdraw(cells):
+         stdscr.erase()
+         stdscr.border()
+         for x, y in cells:
+            stdscr.addch(y+1, x+1, ord('0'))
+         stdscr.refresh()
 
-        curses.curs_set(0)
-        curses.noecho()
-        curses.cbreak()
+      curses.curs_set(0)
+      curses.noecho()
+      curses.cbreak()
 
-        try:
-            simulation = life.Simulation(width, height)
-            simulation.run(itermax, cursesdraw)
-        except KeyboardInterrupt:
-            pass
-        except:
-            curses.endwin()
-            print(traceback.format_exc())
-            sys.exit(1)
-        finally:
-            curses.nocbreak()
-            curses.echo()
-            curses.endwin()
-    else:
-        width, height = 80, 10
-        itermax = 50
+      try:
+         simulation = life.Simulation(width, height)
+         simulation.run(itermax, cursesdraw)
+      except KeyboardInterrupt:
+         pass
+      except:
+         curses.endwin()
+         print(traceback.format_exc())
+         sys.exit(1)
+      finally:
+         curses.nocbreak()
+         curses.echo()
+         curses.endwin()
+   else:
+      width, height = 80, 10
+      itermax = 50
 
-        def printdraw(cells):
-            print("-" * width)
-            for y in range(height):
-                for x in range(width):
-                    print("0" if (x, y) in cells else " ", sep='', end='')
-                print('')
+      def printdraw(cells):
+         print("-" * width)
+         for y in range(height):
+            for x in range(width):
+               print("0" if (x, y) in cells else " ", sep='', end='')
+            print('')
 
-        simulation = life.Simulation(width, height)
-        simulation.run(itermax, printdraw)
+      simulation = life.Simulation(width, height)
+      simulation.run(itermax, printdraw)
 
-    print(simulation.summary())
+   print(simulation.summary())
 
 if __name__ == "__main__":
-    main()
+   main()
--- tkmain.py
+++ tkmain.py
@@ -2,43 +2,43 @@ from Tkinter import *
 import life
 
 class TkLife(Frame):
-        
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        master.resizable(width=FALSE, height=FALSE)
-        master.title('Life')
-        master.geometry('805x605')
-        self.pack()
-        self.simulation = life.Simulation(80, 60)
-        self.createWidgets()
-        self.draw()
+      
+   def __init__(self, master=None):
+      Frame.__init__(self, master)
+      master.resizable(width=FALSE, height=FALSE)
+      master.title('Life')
+      master.geometry('805x605')
+      self.pack()
+      self.simulation = life.Simulation(80, 60)
+      self.createWidgets()
+      self.draw()
 
-    def createWidgets(self):
-        self.canvas = Canvas(self, width=800, height=600, background='white')
-        self.canvas.pack()
+   def createWidgets(self):
+      self.canvas = Canvas(self, width=800, height=600, background='white')
+      self.canvas.pack()
 
-    def draw(self):
-        cells = self.simulation.universe.cells
-        cellsize = 10
-        self.canvas.delete(ALL)
-        for x, y in cells:
-            x1 = x * cellsize
-            y1 = y * cellsize
-            x2 = x1 + cellsize
-            y2 = y1 + cellsize
-            self.canvas.create_rectangle(x1, y1, x2, y2, fill='gray')
+   def draw(self):
+      cells = self.simulation.universe.cells
+      cellsize = 10
+      self.canvas.delete(ALL)
+      for x, y in cells:
+         x1 = x * cellsize
+         y1 = y * cellsize
+         x2 = x1 + cellsize
+         y2 = y1 + cellsize
+         self.canvas.create_rectangle(x1, y1, x2, y2, fill='gray')
 
-    def nextstate(self):
-        self.simulation.universe.nexttick()
+   def nextstate(self):
+      self.simulation.universe.nexttick()
 
 root = Tk()
 
 app = TkLife(master=root)
 
 def simstep():
-    app.nextstate()
-    app.draw()
-    root.after(250, simstep)
+   app.nextstate()
+   app.draw()
+   root.after(250, simstep)
 
 root.after(250, simstep)
 app.mainloop()
