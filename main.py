1f9d1366426c3cf16f7519bec409a081b3ac41ac
Brent Atkinson <brent.atkinson@gmail.com>
2014-04-23 14:53:06 -0400

    Split the main entry points into different files.

--- main.py
+++ main.py
@@ -6,56 +6,18 @@ import life
 
 def main():
 
-   use_curses = True
+   width, height = 80, 10
+   iter_max = 50
 
-   if use_curses:
-      std_scr = curses.initscr()
-      iter_max = None
+   def print_draw(universe):
+      print("-" * width)
+      for y in range(height):
+         for x in range(width):
+            print("0" if (x, y) in universe.cells else " ", sep='', end='')
+         print('')
 
-      def curses_draw(universe):
-         std_scr.erase()
-         std_scr.border()
-         for x, y in universe.cells:
-            std_scr.addch(y+1, x+1, ord('0'))
-         std_scr.refresh()
-
-      # Hide cursor
-      curses.curs_set(0)
-
-      # Don't echo typed characters to screen
-      curses.noecho()
-
-      # Turn off line buffering of input
-      curses.cbreak()
-
-      try:
-         height, width = std_scr.getmaxyx()
-         simulation = life.Simulation(width-2, height-2)
-         simulation.run(iter_max, curses_draw)
-      except KeyboardInterrupt:
-         pass
-      except:
-         curses.endwin()
-         print(traceback.format_exc())
-         sys.exit(1)
-      finally:
-         # Return the terminal to a sane state before exiting
-         curses.nocbreak()
-         curses.echo()
-         curses.endwin()
-   else:
-      width, height = 80, 10
-      iter_max = 50
-
-      def print_draw(universe):
-         print("-" * width)
-         for y in range(height):
-            for x in range(width):
-               print("0" if (x, y) in universe.cells else " ", sep='', end='')
-            print('')
-
-      simulation = life.Simulation(width, height)
-      simulation.run(iter_max, print_draw)
+   simulation = life.Simulation(width, height)
+   simulation.run(iter_max, print_draw)
 
    print(simulation.summary())
 
